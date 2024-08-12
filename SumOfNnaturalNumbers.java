cf6cd3cb48e23af2d67e020f302058f984a3d764
Rajat Tiwari <44523388+rajatreigns@users.noreply.github.com>
2024-08-13 01:14:21 +0530

    Create SumOfNnaturalNumbers.java

---
+++ SumOfNnaturalNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class NaturalNumbers {
+  public static void main(String args[]){
+
+    Scanner sc= new Scanner(System.in);
+    int n= sc.nextInt();
+    int ans= n*(n+1)/2;
+    System.out.println(ans);
+
+  }
+}
+
+//Solution for Sum of first N natural numbers
