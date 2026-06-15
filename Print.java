17d88e8eb6b66e3f15569fb46986a94aeb3e608e
rajatreigns <rajatreigns@gmail.com>
2026-06-16 01:19:13 +0530

    basic  questions

---
+++ Print.java
@@ -0,0 +1,8 @@
+public class Print {
+    public static void main(String args[]){
+        System.out.println("Age: "+25);
+        System.out.println("Name :" +"Rajat");
+        System.out.println("City:"+ "Mumbai");
+    }
+    
+}
---
+++ twonumbersum3.java
@@ -0,0 +1,16 @@
+public class twonumbersum3 {
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
+
+        int c,d,e,sum;
+        c=25;
+        d=35;
+        e=45;
+        sum=a+b+c+d+e;
+        System.out.println(sum);
+
+    }
+    
+}
