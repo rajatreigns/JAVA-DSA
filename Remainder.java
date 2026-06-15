5a7826514814c029b49a9fc172195ac45a10f777
rajatreigns <rajatreigns@gmail.com>
2026-06-16 03:17:22 +0530

    Add Remainder.java

---
+++ Remainder.java
@@ -0,0 +1,14 @@
+public class Remainder {
+    public static void main(String args[]){
+        int a,b,c,d;
+        a=100;
+        b=4;
+        c=a/b;
+        d=a%b;
+        System.out.println(c);
+                System.out.println(d);
+
+    }
+
+    
+}
