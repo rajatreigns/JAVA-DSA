dd8b784950f32989d9e9fe872498d969d2896432
rajatreigns <rajatreigns@gmail.com>
2026-06-17 00:28:13 +0530

    Add Swapp.java

---
+++ Swapp.java
@@ -0,0 +1,12 @@
+public class Swapp{
+    public static void main(String args[]){
+        int a,b,c;
+        a=1;
+        b=4;
+        c=a-b;
+        a=b;
+        b=c+a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
