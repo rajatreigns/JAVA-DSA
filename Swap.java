e565dc42beba26d5d2129606b7a8c45b5da2c171
rajatreigns <rajatreigns@gmail.com>
2026-06-16 23:11:52 +0530

    Add Swap.java

---
+++ Swap.java
@@ -0,0 +1,13 @@
+public class Swap{
+    public static void main(String args[]){
+        int a, b, c;
+        a=10;
+        b=20;
+        c=30;
+        c=a;
+        a=b;
+        b=c;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
